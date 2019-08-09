6bd4c562fe0b019f07a760f44359337de553ae21
ZacharyBoom <43230741+ZacharyBoom@users.noreply.github.com>
2019-08-09 11:31:32 +0800

    firstCommit

---
+++ Example/Pods/Target Support Files/Pods-YzlLiteAVManager_Example/Pods-YzlLiteAVManager_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YzlLiteAVManager_Example : NSObject
+@end
+@implementation PodsDummy_Pods_YzlLiteAVManager_Example
+@end
---
+++ Example/Pods/Target Support Files/YzlLiteAVManager/YzlLiteAVManager-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_YzlLiteAVManager : NSObject
+@end
+@implementation PodsDummy_YzlLiteAVManager
+@end
